2dc2623356dbe0f3e738321134ebf04a6d96ea13
王一杨 <wangyiyang@163.com>
2018-05-15 09:30:04 +0800

    引入antd-mobile

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -137,9 +137,81 @@ module.exports = {
         // "oneOf" will traverse all following loaders until one will
         // match the requirements. When no loader matches it will fall
         // back to the "file" loader at the end of the loader list.
+        
         oneOf: [
           // "url" loader works just like "file" loader but it also embeds
           // assets smaller than specified size as data URLs to avoid requests.
+          /**************引入 antd-mobile *******************/
+          {
+            test: /\.css$/,
+            exclude: /node_modules|antd-mobile\.css/,
+            use: [
+              require.resolve('style-loader'),
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  modules: true,
+                  importLoaders: 1,
+                  localIdentName: '[local]___[hash:base64:5]'
+                },
+              },
+              {
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  // Necessary for external CSS imports to work
+                  // https://github.com/facebookincubator/create-react-app/issues/2677
+                  ident: 'postcss',
+                  plugins: () => [
+                    require('postcss-flexbugs-fixes'),
+                    autoprefixer({
+                      browsers: [
+                        '>1%',
+                        'last 4 versions',
+                        'Firefox ESR',
+                        'not ie < 9', // React doesn't support IE8 anyway
+                      ],
+                      flexbox: 'no-2009',
+                    }),
+                  ],
+                },
+              },
+            ]
+          },
+          {
+            test: /\.less$/,
+            use: [
+              require.resolve('style-loader'),
+              require.resolve('css-loader'),
+              {
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  // Necessary for external CSS imports to work
+                  // https://github.com/facebookincubator/create-react-app/issues/2677
+                  ident: 'postcss',
+                  plugins: () => [
+                    require('postcss-flexbugs-fixes'),
+                    autoprefixer({
+                      browsers: [
+                        '>1%',
+                        'last 4 versions',
+                        'Firefox ESR',
+                        'not ie < 9', // React doesn't support IE8 anyway
+                      ],
+                      flexbox: 'no-2009',
+                    }),
+                  ],
+                },
+              },
+              {
+                loader: require.resolve('less-loader'),
+                options: {
+                  // theme vars, also can use theme.js instead of this.
+                  modifyVars: { "@brand-primary": "#74b9ff" },
+                },
+              },
+            ]
+          },
+          /*********************************************************/
           {
             test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
             loader: require.resolve('url-loader'),
@@ -154,7 +226,10 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              
+              plugins: ['transform-runtime', ['import', {
+                libraryName: 'antd-mobile',
+                style: true
+              }]],
               compact: true,
             },
           },
--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 import styles from './App.css';
 import CSSModules from 'react-css-modules';
+
+//引入
+import { NavBar, Icon, Button } from 'antd-mobile';
 class App extends Component {
   render() {
     return (
       <div styleName="App">
-        hello vw-layout
+        <NavBar
+        mode="dark"
+        icon={<Icon type="left" />}
+        onLeftClick={() => console.log('onLeftClick')}
+        rightContent={[
+          <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
+          <Icon key="1" type="ellipsis" />,
+        ]}
+      >NavBar</NavBar>
+      <Button onClick={(e) => this.getNum(e)} type="warning" inline size="small" style={{ marginRight: '4px' }}>ghost</Button>
+      <NavBar
+        mode="dark"
+        leftContent="Back"
+        rightContent={[
+          <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
+          <Icon key="1" type="ellipsis" />,
+        ]}
+      >NavBar</NavBar>
       </div>
     );
+  }
+  getNum(e) {
+    let i = 0;
+    console.log(i++);
   }
 }
 
