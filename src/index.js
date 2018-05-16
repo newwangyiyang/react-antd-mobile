95bf081f0f1e186e50d3e80b2a81a11b752a681c
王一杨 <wangyiyang@163.com>
2018-05-16 13:26:31 +0800

    增加初始化样式

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import Route from './route/index';
+import './assets/css/reset.css';
 ReactDOM.render(
     <Route />, 
     document.getElementById('root'));
